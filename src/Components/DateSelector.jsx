eaea3d87e5988fec84d2c12b8efaead3b6567502
Zac Long <zac.long.96@gmail.com>
2023-03-23 18:20:40 +1100

    added next and previous date changing buttons, still needs a little bit more polish

--- src/Components/DateSelector.jsx
+++ src/Components/DateSelector.jsx
@@ -5,17 +5,36 @@ const DateSelector = ({ selectedDate, onDateChange}) => {
     onDateChange(e.target.value)
   }
 
+  const handlePreviousDayClick = () => {
+    const currentDate = new Date(selectedDate)
+    currentDate.setDate(currentDate.getDate() - 1)
+    onDateChange(currentDate.toISOString().split('T')[0])
+  }
+
+  const handleNextDayClick = () => {
+    const currentDate = new Date(selectedDate)
+    currentDate.setDate(currentDate.getDate() + 1)
+    onDateChange(currentDate.toISOString().split('T')[0])
+  }
+
+
   return (
     <div>  
-      <form>
-        <label htmlFor="date">Select a date: </label>
-        <input 
-        type="date" 
-        id="date-selector" 
-        value={selectedDate}
-        onChange={handleChange}
-        />
-      </form>
+      <div>
+        <form>
+          <label htmlFor="date">Select a date: </label>
+          <input 
+          type="date" 
+          id="date-selector" 
+          value={selectedDate}
+          onChange={handleChange}
+          />
+        </form>
+        <div>
+          <button onClick={handlePreviousDayClick}>Previous day</button>
+          <button onClick={handleNextDayClick}>Next day</button>
+        </div>
+      </div>
       <p><strong>Note:</strong> dates must be after the start of the 1979 season. Some stats from older games may be missing some data.</p>
     </div>
   )
